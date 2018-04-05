d284b62f979a66b6a9f4964be11edb37243de206
Jia <jiawu17@gmail.com>
2018-04-05 18:40:14 -0400

    Added README and tidied up setups

---
+++ README.md
@@ -0,0 +1,44 @@
+# Masternode Setup Script
+Te bash scripts will go through the setup process to create the masternode coin daemon.
+
+### git_setup.sh
+###### Run this if you want to compile from source code
+1. Asks for github link.
+2. Asks for private key. Get this from your main wallet by running ```masternode genkey``` in your debug window or cli
+3. Checks if 1.5GB of memory is available. If not, attempts to create a 2GB swapfile.
+4. Updates and upgrades system.
+5. Installs required packages.
+6. Pulls from github and compiles coin.
+7. Sets up the coin daemon conf file.
+
+### wget_setup.sh - TODO
+###### Run this if you want to already have a daemon
+1. Asks for zip or tar url. 
+If the files are already extracted into the current directory, leave this blank.
+2. Asks for private key. Get this from your main wallet by running ```masternode genkey``` in your debug window or cli
+3. Checks if 0.5GB of memory is available. If not, attempts to create a 2GB swapfile.
+4. Updates and upgrades system.
+5. Installs required packages.
+6. Wgets url and extracts.
+7. Sets up the coin daemon conf file.
+
+Example .conf
+```
+rpcuser=servepeaku
+rpcpassword=servepeakp
+rpcallowip=127.0.0.1
+
+listen=1
+server=1
+daemon=1
+
+logtimestamps=1
+maxconnections=256
+
+masternode=1
+externalip=0.0.0.0
+bind=0.0.0.0
+masternodeprivkey=7rRc1kGaYkGeUNp2ZFPRaWqKxGd5hSDVTgqsPK9NPs8mcaAzZ4E
+```
+
+Donate via BTC: [35K33YaAjsq1trdynNeeQfisnQXXxjaSEF](bitcoin:35K33YaAjsq1trdynNeeQfisnQXXxjaSEF)
---
+++ git_setup.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+output() {
+    printf "\E[0;33;40m"
+    echo $1
+    printf "\E[0m"
+}
+
+displayErr() {
+    echo
+    echo $1;
+    echo
+    exit 1;
+}
+
+output "Created by ServePeak. Used to automatically setup masternodes on daemon end."
+output "Let's begin the install process."
+
+read -e -p "What is the GitHub URL for the coin?: " github
+folder="$(echo $github | cut -d'/' -f5 | cut -d'.' -f 1)"
+read -e -p "What is your private key?: " privkey
+initial=$PWD
+
+output "Checking total system memory."
+    output ""
+    sleep 3
+    ram="$(awk '/MemTotal/ {print $2}' /proc/meminfo)"
+    swap="$(awk '/SwapTotal/ {print $2}' /proc/meminfo)"
+    total="$(expr $ram + $swap)"
+    if [ "$total" -le "1572864" ]; then
+        output "Not enough memory, making swapfile."
+            output ""
+            sleep 3
+            dd if=/dev/zero of=/swapfile count=2048 bs=1M
+            chmod 600 /swapfile
+            mkswap /swapfile
+            swapon /swapfile
+            swap="$(awk '/SwapTotal/ {print $2}' /proc/meminfo)"
+            total="$(expr $ram + $swap)"
+            if [ "$total" -le "1572864" ]; then
+                read -e -p "Could not allocate swap. Do you want to try and continue anyways? Y/n: " swapcon
+                if [[ ("$swapcon" == "n" || "$swapcon" == "N") ]]; then
+                    free -m
+                    output "Requirement: 1500 MB of Mem + Swap."
+                    exit 1
+                fi
+            else
+                echo "/swapfile   none    swap    sw    0   0" > /etc/fstab
+            fi
+    fi
+
+if [ -z "$(dpkg -l | grep libgmp-dev)" ]; then
+
+
+output "Updating and upgrading system."
+    output ""
+	sleep 3
+    
+    sudo apt-get -y update 
+    sudo apt-get -y upgrade
+    sudo apt-get -y autoremove
+
+output "Installing required packages for daemons."
+    output ""
+    sleep 3  
+
+    sudo apt-get -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+    sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+    sudo apt-get -y install libboost-all-dev
+    sudo apt-get -y install software-properties-common
+    sudo add-apt-repository -y ppa:bitcoin/bitcoin
+    sudo apt-get update
+    sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
+    sudo apt-get -y install libminiupnpc-dev
+    sudo apt-get -y install libzmq3-dev
+    sudo apt-get -y install libgmp-dev
+
+fi 
+
+output "Compiling Coin"
+    output ""
+	sleep 3
+    sudo git clone $github
+    cd $folder
+    sudo chmod -R 777 *
+    cd src
+    if [ -f "makefile.unix" ]; then
+        sudo make -f makefile.unix
+    else
+        cd ..
+        sudo ./autogen.sh
+        sudo ./configure
+        sudo make
+        cd src
+    fi
+    
+output "Setting Up Masternode"
+    output ""
+	sleep 3
+    coin="$(find *d ! -name *.*d)"
+    location=$PWD
+    ./$coin
+    pid="pgrep $coin"
+    sleep 30
+    kill -9 $pid
+    cd 
+    datadir="$(find -type d -name .${coin::-1})"
+    cd $datadir
+    config="$(find *.conf ! -name masternode.conf)"
+    ip="$(curl ipecho.net/plain)"
+    echo "rpcuser=servepeaku" >> $config
+	echo "rpcpassword=servepeakp" >> $config
+    echo "rpcallowip=127.0.0.1" >> $config
+    echo "" >> $config
+    echo "listen=1" >> $config
+    echo "server=1" >> $config
+    echo "daemon=1" >> $config
+    echo "" >> $config
+	echo "logtimestamps=1" >> $config
+	echo "maxconnections=256" >> $config
+    echo "" >> $config
+	echo "masternode=1" >> $config
+	echo "externalip=${ip}" >> $config
+    echo "bind=${ip}" >> $config
+    echo "masternodeprivkey=${privkey}" >> $config
+    cd $location
+    ./$coin
+    cd $initial
+
+output "Done! If your daemon does not sync you need to addnode= in your .{coinname}/{coinname}.conf"
+output " "
